275f65350275bd98a62a3069a483ea21443f0272
Courtney Simonse <courtney.r.elder@gmail.com>
2022-04-06 10:30:48 -0400

    add state and congressional district boundaries

--- js/app.js
+++ js/app.js
@@ -19,81 +19,10 @@
   	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
-  // var Mapbox = {
-	// 	url: 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}{r}?access_token={accessToken}',
-	// 	options: {
-	// 		attribution:
-	// 			'&copy; <a href="https://www.mapbox.com/about/maps/" target="_blank">Mapbox</a> ' +
-	// 			'{attribution.OpenStreetMap} ' +
-	// 			'<a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a>',
-	// 		tileSize: 512,
-	// 		maxZoom: 18,
-	// 		zoomOffset: -1,
-	// 		id: 'mapbox/satellite-streets-v11',
-	// 		accessToken: 'pk.eyJ1IjoiY291cnRuZXlzaW1vbnNlIiwiYSI6ImNqZGozNng0NjFqZWIyd28xdDJ2MXduNTcifQ.PoSFtqfsq1di1IDXzlN4PA',
-	// 	}
-	// }
-
-  // L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}{r}?access_token={accessToken}', {
-  //   attribution:
-  //     '&copy; <a href="https://www.mapbox.com/about/maps/" target="_blank">Mapbox</a> ' +
-  //     '{attribution.OpenStreetMap} ' +
-  //     '<a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a>',
-  //   tileSize: 512,
-  //   maxZoom: 18,
-  //   zoomOffset: -1,
-  //   id: 'mapbox/satellite-streets-v11',
-  //   accessToken: 'pk.eyJ1IjoiY291cnRuZXlzaW1vbnNlIiwiYSI6ImNqZGozNng0NjFqZWIyd28xdDJ2MXduNTcifQ.PoSFtqfsq1di1IDXzlN4PA'
-  // }).addTo(map);
-
   // change zoom control position
   L.control.zoom({
     position: 'bottomleft'
   }).addTo(map);
-
-  // setup slider
-  var dateSlider = document.getElementById('slider');
-
-  function timestamp(str) {
-      return new Date(str).getTime();
-  }
-
-  noUiSlider.create(dateSlider, {
-  // Create two timestamps to define a range.
-      range: {
-          min: timestamp('2010'),
-          max: timestamp('2016')
-      },
-
-  // Steps of one week
-      step: 7 * 24 * 60 * 60 * 1000,
-
-  // Two more timestamps indicate the handle starting positions.
-      start: [timestamp('2011-01-15')],
-
-  // No decimals
-      format: {
-        to: function (value) {
-          return value;
-        },
-        from: function (value) {
-          return Number(value);
-        }
-      }
-  });
-
-
-  // update slider
-  var dateValues = [
-      document.getElementById('event')
-  ];
-
-  var formatter = new Intl.DateTimeFormat('en-US');
-
-  dateSlider.noUiSlider.on('update', function (values, handle) {
-      dateValues[handle].innerHTML = formatter.format(new Date(+values[handle]));
-  });
-
 
 
   // GET DATA
@@ -114,6 +43,25 @@
 
   // DRAW MAP FUNCTION
   function drawMap() {
+
+    var statesLayer = L.geoJson(states, {
+      style: function (feature) {
+        return {
+          color: 'black',
+          weight: 2,
+          fill: false
+        }
+      }
+    }).addTo(map);
+    var cdLayer = L.geoJson(cd2020, {
+      style: function (feature) {
+        return {
+          color: 'black',
+          weight: 1,
+          fill: false
+        }
+      }
+    }).addTo(map);
 
   }   //end drawMap()
 
